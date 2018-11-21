5e4dce43a05c3278c1eee657e01c1947aeb1d053
Arwa-Ibrahim <32910693+Arwa-Ibrahim@users.noreply.github.com>
2018-11-21 21:42:39 +0200

    Update Alter.py

--- CRC/Alter.py
+++ CRC/Alter.py
@@ -1,14 +1,15 @@
 def Alter (message_generator , to_change_bit):
 
-    # Reverse the required bit
-    reversed_bit = message_generator[to_change_bit-1]
-    if reversed_bit == '1' : 
+    # Get required bit
+    reversed_bit = message_generator[int(to_change_bit)-1]
+    
+    # XOR the bit we want to reverse
+    if reversed_bit == '1' :
         reversed_bit = '0'
-    elif reversed_bit == '0' : 
+    elif reversed_bit == '0' :
         reversed_bit = '1'
+        
     # The edited message
-    message_recieved = message_generator [0:to_change_bit-1] + reversed_bit + message_generator [to_change_bit:]
+    message_recieved = message_generator [0:int(to_change_bit)-1] + reversed_bit + message_generator [int(to_change_bit):]
     
     return message_recieved 
-
-
